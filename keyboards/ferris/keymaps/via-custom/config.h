84c015418be2788bca03cc0955955a49ed179cb5
Matjaž Finžgar <matjaz@finzgar.net>
2023-02-24 09:30:54 +0100

    add custom via configs for pegasushoof and ferris

---
+++ keyboards/ferris/keymaps/via-custom/config.h
@@ -0,0 +1,12 @@
+// Copyright 2020 Pierre Chevalier <pierrechevalier83@gmail.com>
+// SPDX-License-Identifier: GPL-2.0+
+
+#pragma once
+
+// Good defaults for home row modifiers
+#define TAPPING_TERM 230
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_LED_STATE_ENABLE
+#define SPLIT_TRANSPORT_MIRROR
---
+++ keyboards/ferris/keymaps/via-custom/rules.mk
@@ -0,0 +1,4 @@
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+BOOTLOADER = atmel-dfu
+VIA_ENABLE = yes
+LTO_ENABLE = yes
