ee7fb679b1b2e15d19a0ac9aef958f01ddb9c897
Matjaž Finžgar <matjaz@finzgar.net>
2023-03-06 14:47:56 +0100

    fix layout

--- keyboards/ferris/keymaps/custom/keymap.c
+++ keyboards/ferris/keymaps/custom/keymap.c
@@ -115,9 +115,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_NUM] = LAYOUT(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_7, KC_8, KC_9, KC_TRNS,
-    KC_TRNS, KC_LCTL, KC_LGUI, KC_LALT, KC_TRNS,         KC_TRNS, KC_4, KC_5, KC_6, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_0,    KC_1, KC_2, KC_3, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_PSLS, KC_7, KC_8, KC_9, KC_PMNS,
+    KC_TRNS, KC_LCTL, KC_LGUI, KC_LALT, KC_TRNS,         KC_PAST, KC_4, KC_5, KC_6, KC_PPLS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_0,    KC_1, KC_2, KC_3, KC_DOT,
                                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
