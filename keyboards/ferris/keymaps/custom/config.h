39559ab8448ff6444ceb985be8440f7b02d7596c
Matjaž Finžgar <matjaz@finzgar.net>
2023-03-06 10:20:49 +0100

    fix layout

--- keyboards/ferris/keymaps/custom/config.h
+++ keyboards/ferris/keymaps/custom/config.h
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 200
+#define TAPPING_TERM 180
 #define IGNORE_MOD_TAP_INTERRUPT
 
 #define SPLIT_LAYER_STATE_ENABLE
