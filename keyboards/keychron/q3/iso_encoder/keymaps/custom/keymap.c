eb17e8652440cea87660d5f2942d2b5636ecb44b
Matjaž Finžgar <matjaz@finzgar.net>
2023-10-13 11:15:52 +0200

    fix: support for macOS

--- keyboards/keychron/q3/iso_encoder/keymaps/custom/keymap.c
+++ keyboards/keychron/q3/iso_encoder/keymaps/custom/keymap.c
@@ -27,23 +27,44 @@ enum custom_keycodes {
     MAC_ZIN = SAFE_RANGE,
     MAC_ZOUT,
     MAC_ZRES,
+    WIN_ZIN,
+    WIN_ZOUT,
+    WIN_ZRES
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case MAC_ZIN:
         if (record->event.pressed) {
-            SEND_STRING(SS_LCTL("="));
+            SEND_STRING(SS_LCMD("="));
         }
         break;
 
     case MAC_ZOUT:
         if (record->event.pressed) {
-            SEND_STRING(SS_LCTL("-"));
+            SEND_STRING(SS_LCMD("-"));
         }
         break;
 
     case MAC_ZRES:
+        if (record->event.pressed) {
+           SEND_STRING(SS_LCMD("0"));
+        }
+        break;
+
+    case WIN_ZIN:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LCTL("="));
+        }
+        break;
+
+    case WIN_ZOUT:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LCTL("-"));
+        }
+        break;
+
+    case WIN_ZRES:
         if (record->event.pressed) {
            SEND_STRING(SS_LCTL("0"));
         }
@@ -54,23 +75,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_tkl_f13_iso(
-        KC_ESC,   KC_BRID,  KC_BRIU,  KC_NO,    KC_NO,    RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    KC_MUTE,  KC_NO,    KC_NO,    RGB_MOD,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,              KC_DEL,   KC_END,   KC_PGDN,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,    KC_ENT,
-        KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,
-        KC_LCTL,  KC_LOPT,  KC_LCMD,                                KC_SPC,                                 KC_RCMD,  KC_ROPT,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_ESC,         KC_F20,   KC_CALC,  KC_MAIL,  _______,  KC_BRID,  KC_BRIU,  RGB_VAD,  RGB_VAI,  KC_MPLY,  KC_MPRV,  KC_MNXT,  KC_MSTP,  KC_MUTE,  KC_PSCR,  _______,  KC_PAUS,
+        KC_GRV,         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
+        KC_TAB,         KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,            KC_DEL,   KC_END,   KC_PGDN,
+        LT(1,KC_CAPS),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,
+        KC_LSFT,        KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,            KC_UP,
+        KC_LCTL,        KC_LOPT,  KC_LCMD,                                KC_SPC,                                 KC_RCMD,  KC_ROPT,  MO(1),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_tkl_f13_iso(
-        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     RGB_TOG,  _______,  _______,  RGB_TOG,
+        QK_BOOT,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     MAC_ZRES, _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,  _______,  _______,
+        _______,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______,  _______,  _______,              _______,  _______,  _______,
         _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,  _______,    _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______,  _______,              _______,            _______,
-        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    RGB_TOG,  _______,  _______,  _______),
+        _______,  _______,  _______,  _______,  _______,  _______,  KC_INS,   KC_DEL,   _______,  _______,  _______,  _______,              _______,            _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  KC_TRNS,    RGB_TOG,  _______,  _______,  _______),
 
     [WIN_BASE] = LAYOUT_tkl_f13_iso(
-        KC_ESC,         KC_F20,   KC_CALC,  KC_MAIL,  _______,  KC_BRID,  KC_BRIU,  _______,  _______,  KC_MPLY,  KC_MPRV,  KC_MNXT,  KC_MSTP, KC_MUTE,  KC_PSCR,  _______,  KC_PAUS,
+        KC_ESC,         KC_F20,   KC_CALC,  KC_MAIL,  _______,  KC_BRID,  KC_BRIU,  RGB_VAD,  RGB_VAI,  KC_MPLY,  KC_MPRV,  KC_MNXT,  KC_MSTP, KC_MUTE,  KC_PSCR,  _______,  KC_PAUS,
         KC_GRV,         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
         KC_TAB,         KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,           KC_DEL,   KC_END,   KC_PGDN,
         LT(3,KC_CAPS),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS, KC_ENT,
@@ -78,9 +99,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,        KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(3),   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_tkl_f13_iso(
-        QK_BOOT,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     MAC_ZRES,  _______,  _______,  _______,
+        QK_BOOT,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     WIN_ZRES,  _______,  _______,  _______,
         _______,  KC_F13,   KC_F14,   KC_F15,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,   _______,  _______,  _______,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______,  _______,  _______,               _______,  _______,  _______,
+        _______,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______,  _______,  _______,               _______,  _______,  _______,
         _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,  _______,    _______,
         _______,  _______,  _______,  _______,  _______,  _______,  KC_INS,   KC_DEL,   _______,  _______,  _______,  _______,              _______,             _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  KC_TRNS,    RGB_TOG,   _______,  _______,  _______),
@@ -89,9 +110,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][1][2] = {
     [MAC_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [MAC_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
+    [MAC_FN]   = {ENCODER_CCW_CW(MAC_ZOUT, MAC_ZIN) },
     [WIN_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [WIN_FN]   = {ENCODER_CCW_CW(MAC_ZOUT, MAC_ZIN) }
+    [WIN_FN]   = {ENCODER_CCW_CW(WIN_ZOUT, WIN_ZIN) }
 };
 #endif
 
