686e7b69dc3aae701fec6188940660b361ca5cc2
Matjaž Finžgar <matjaz@finzgar.net>
2023-10-25 22:18:31 +0200

    feat: add meh key to Sweep

--- keyboards/ferris/keymaps/custom/keymap.c
+++ keyboards/ferris/keymaps/custom/keymap.c
@@ -44,7 +44,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
         break;
 
-
     case CK_CLOSE:
         if (record->event.pressed) {
             SEND_STRING(SS_LCTL(SS_TAP(X_W)));
@@ -97,39 +96,41 @@ enum ferris_layers {
 #define ENT_1 LT(1, KC_ENT)
 #define BSPC_SF RSFT_T(KC_BSPC)
 #define TAB_3 LT(3, KC_TAB)
+#define A_MEH MEH_T(KC_A)
 #define S_CTL LCTL_T(KC_S)
 #define D_OPT OPT_T(KC_D)
 #define F_CMD LCMD_T(KC_F)
 #define J_CMD LCMD_T(KC_J)
 #define K_OPT OPT_T(KC_K)
 #define L_CTL RCTL_T(KC_L)
+#define SCLN_MEH MEH_T(KC_SCLN)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT(
-    KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,            KC_Y, KC_U,  KC_I,    KC_O,   KC_P,
-    KC_A,  S_CTL, D_OPT, F_CMD, KC_G,            KC_H, J_CMD, K_OPT,   L_CTL,  KC_SCLN,
-    KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,            KC_N, KC_M,  KC_COMM, KC_DOT, KC_SLSH,
-                            TAB_3, ENT_1, SPC_2, BSPC_SF
+    KC_Q,   KC_W,  KC_E,  KC_R,  KC_T,            KC_Y, KC_U,  KC_I,    KC_O,   KC_P,
+    A_MEH,  S_CTL, D_OPT, F_CMD, KC_G,            KC_H, J_CMD, K_OPT,   L_CTL,  SCLN_MEH,
+    KC_Z,   KC_X,  KC_C,  KC_V,  KC_B,            KC_N, KC_M,  KC_COMM, KC_DOT, KC_SLSH,
+                             TAB_3, ENT_1, SPC_2, BSPC_SF
   ),
 
   [_NAV] = LAYOUT(
     _______,  _______, _______, _______, _______,         _______, _______, KC_INS,  _______, _______,
-    _______,  KC_LCTL, KC_LOPT, KC_LCMD, _______,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+    KC_MEH,   KC_LCTL, KC_LOPT, KC_LCMD, _______,         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
     _______,  _______, _______, _______, _______,         KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,
                                     _______, _______, MO(4), _______
   ),
 
   [_SYM] = LAYOUT(
     _______, _______, KC_EQL,  KC_MINS, KC_NUHS,         _______, _______, _______, _______, _______,
-    KC_GRV,  KC_LBRC, KC_LCBR, KC_LPRN, KC_QUOT,         _______, KC_LCMD, KC_LOPT, KC_LCTL, _______,
+    KC_GRV,  KC_LBRC, KC_LCBR, KC_LPRN, KC_QUOT,         _______, KC_LCMD, KC_LOPT, KC_LCTL, KC_MEH,
     KC_NUBS, KC_RBRC, KC_RCBR, KC_RPRN, CK_DQUO,         _______, _______, _______, _______, _______,
                                      _______, MO(4), _______, _______
   ),
 
   [_NUM] = LAYOUT(
     _______, _______, _______, _______, _______,         KC_PSLS, KC_7, KC_8, KC_9, KC_PMNS,
-    _______, KC_LCTL, KC_LOPT, KC_LCMD, _______,         KC_PAST, KC_4, KC_5, KC_6, KC_PPLS,
+    KC_MEH,  KC_LCTL, KC_LOPT, KC_LCMD, _______,         KC_PAST, KC_4, KC_5, KC_6, KC_PPLS,
     _______, _______, _______, _______, _______,         KC_0,    KC_1, KC_2, KC_3, KC_DOT,
                                      _______, _______, _______, _______
   ),
